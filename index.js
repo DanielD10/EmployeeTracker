2cca35f0a7adf38afada471e9df7fc6c24278d7d
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-06-05 02:47:14 -0500

    finished adding options

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function start() {
     })
     // if they choose to add a department then ask for department name.
     .then(function (answer) {
-        console.log(answer)
+      console.log(answer);
       if (answer.name === "add Department") {
         inquirer
           .prompt({
@@ -44,22 +44,117 @@ function start() {
               {
                 name: answer.Department,
               },
-              
+
               function (err, res) {
                 if (err) throw err;
                 start();
               }
             );
           });
-      }else if(answer.name === "add Role"){
-          // ask for role questions
-      }else if(answer.name === "add Employee"){
-          //ask for employee questions
-      }else if (answer.name === "Exit"){
-          connection.end();
+      } else if (answer.name === "add Role") {
+        inquirer
+          .prompt([
+            {
+              name: "title",
+              type: "input",
+              message: "What is your job title?",
+            },
+            {
+              name: "salary",
+              type: "input",
+              message: "What is your year Salary?",
+            },
+            {
+              name: "department_id",
+              type: "list",
+              message: "What department are you in?",
+              choices: ["Production", "Research & development", "Marketing"],
+            },
+          ])
+          .then(function (answer) {
+            var department_id;
+            if (answer.department_id === "Production") {
+              department_id = 1;
+            } else if (answer.department_id === "Research & development") {
+              department_id = 2;
+            } else {
+              department_id = 3;
+            }
+            console.log(answer);
+            connection.query("INSERT INTO roles SET ?", {
+              title: answer.title,
+              salary: answer.salary,
+              department_id: department_id,
+            },
+            function(err,res){
+                if(err) throw err;
+                start();
+            }
+            
+            );
+          });
+        // ask for role questions
+      } else if (answer.name === "add Employee") {
+          inquirer
+          .prompt([
+              {
+                name: "first_name",
+                type: "input",
+                message: "What is your first name?" 
+              },
+              {
+                  name:"last_name",
+                  type: "input",
+                  message: "What is your last name?"
+              },
+              {
+                  name: "role",
+                  type: "list",
+                  message: "What is your role?",
+                  choices: ["Marketing Director", "Research Director", "Web Developer", "Production Manager"]
+              },
+              {
+                name: "manager",
+                type: "list",
+                message: "Do you have a manager?",
+                choices: ["Daniel Mckiney", "Sarah Miller", "None"]
+              }
+          ]).then(function (answer) {
+            var role_id;
+            if (answer.role === "Marketing Director") {
+              role_id = 1;
+            } else if (answer.role === "Research Director") {
+              role_id = 2;
+            } else if (answer.role === "Web Developer") {
+                role_id = 3;
+            } else {
+              role_id = 4;
+            }
+            var manager_id;
+            if (answer.manager === "Daniel Mckiney") {
+              manager_id = 1;
+            } else if (answer.manager === "Sarah Miller") {
+                manager_id = 2;
+            }else {
+                manager_id = null
+            }
+            connection.query("INSERT INTO employees SET ?", {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: role_id,
+                manager_id: manager_id
+              },
+              function(err,res){
+                  if(err) throw err;
+                  start();
+              }
+              
+              )
+          })
+        }
+        else if (answer.name === "Exit") {
+                connection.end();
       }
-    
-      
     });
 }
 
